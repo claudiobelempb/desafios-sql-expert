faa2153dbad81ec3944615252de1ab36308b9b1b
claudiobelempb <claudio.c.lima@hotmail.com>
2024-06-29 14:04:00 -0300

    RESTRIÇÃO (SELEÇÃO)

--- empregados.sql
+++ empregados.sql
@@ -21,5 +21,18 @@ INSERT INTO tb_empregado (id, nome, dept_id) VALUES (2, 'João', 2);
 INSERT INTO tb_empregado (id, nome, dept_id) VALUES (3, 'Ana', 1);
 INSERT INTO tb_empregado (id, nome, dept_id) VALUES (4, 'Carlos', 2);
 
-/*Projeção*/
+/*
+PROJEÇÃO
+É uma operação que "filtra" as colunas da tabela.
+"Projeção das colunas id e nome da tabela tb_empregado"
+*/
 SELECT id, nome FROM tb_empregado;
+
+/*
+RESTRIÇÃO (SELEÇÃO)
+É uma operação que "filtra" as linhas da tabela conforme um predicado.
+"Restrição na tb_empregado das linhas tal que: dept_id = 2"
+*/
+SELECT * FROM tb_empregado WHERE dept_id = 2;
+/*Você pode combinar operações*/
+SELECT id, nome FROM tb_empregado WHERE dept_id = 2;
