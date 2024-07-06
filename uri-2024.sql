07f82facaba5e24948ef86bee5b14a5b04af29a7
claudiobelempb <claudio.c.lima@hotmail.com>
2024-07-06 17:06:10 -0300

    URI 2618 Produtos Importados

--- uri-2024.sql
+++ uri-2024.sql
@@ -124,3 +124,16 @@ SELECT products.name, providers.name FROM products
 INNER JOIN providers
 ON products.id_providers = providers.id 
 WHERE providers.name = 'Ajax SA'
+
+/*
+URI 2618
+Produtos Importados
+O setor de importação da nossa empresa precisa de um relatório sobre a importação de produtos do nosso fornecedor Sansul.
+Sua tarefa é exibir o nome dos produtos, o nome do fornecedor e o nome da categoria, para os produtos fornecidos pelo fornecedor ‘Sansul SA’ e cujo nome da categoria seja 'Imported'.
+*/
+SELECT products.name, providers.name, categories.name  FROM products
+INNER JOIN categories
+ON products.id_categories = categories.id
+INNER JOIN providers
+ON products.id_providers = providers.id
+WHERE providers.name = 'Sansul SA' AND categories.name = 'Imported'
