7aa1858e18423430007d01fdfd9e271ce64e9ec0
claudiobelempb <claudio.c.lima@hotmail.com>
2024-07-06 14:43:23 -0300

    URI 2608 Maior e Menor Preço

--- uri-2024.sql
+++ uri-2024.sql
@@ -50,3 +50,16 @@ SELECT products.id, products.name FROM products
 INNER JOIN categories
 ON products.id_categories = categories.id
 WHERE UPPER(categories.name) LIKE UPPER('super%')
+
+/*
+URI 2607
+Cidades em Ordem Alfabética
+Todos os meses a empresa pede um relatório das cidades que os fornecedores estão cadastrados. 
+Dessa vez não vai ser diferente, faça uma consulta no Banco de Dados que retorne todas as cidades dos fornecedores, 
+mas em ordem alfabética.
+OBS: Você não deve mostrar cidades repetidas.
+*/
+SELECT products.id, products.name FROM products
+INNER JOIN categories
+ON products.id_categories = categories.id
+WHERE UPPER(categories.name) LIKE UPPER('super%')
