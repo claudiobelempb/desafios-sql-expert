3dea407d0d9c7e3673a32622ee10c5ad7ed96907
claudiobelempb <claudio.c.lima@hotmail.com>
2024-07-06 15:39:12 -0300

    URI 2610 Valor Médio dos Produtos

--- uri-2024.sql
+++ uri-2024.sql
@@ -82,3 +82,16 @@ INNER JOIN categories
 ON products.id_categories = categories.id
 GROUP BY categories.name
 ORDER BY categories.name;
+
+/*
+URI 2610
+Valor Médio dos Produtos
+Na empresa que você trabalha está sendo feito um levantamento sobre os valores dos produtos que são comercializados.
+Para ajudar o setor que está fazendo esse levantamento você deve calcular e exibir o valor médio do preço dos produtos.
+OBS: Mostrar o valor com dois números após o ponto.
+*/
+SELECT categories.name, SUM(products.amount) FROM products
+INNER JOIN categories
+ON products.id_categories = categories.id
+GROUP BY categories.name
+ORDER BY categories.name;
